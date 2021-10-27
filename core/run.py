66822cc4ab88c0db10bbbb5f9f7347e024515905
Giovanni De Toni <giovanni.det@gmail.com>
2021-10-27 09:49:13 +0200

    Add a trainer class to manage the training step.

--- core/run.py
+++ core/run.py
@@ -2,6 +2,7 @@ from core.mcts_exact import MCTSExact
 from agents.standard_policy import StandardPolicy
 from environments.mock_env import MockEnv, MockEnvEncoder
 from core.buffer.trace_buffer import PrioritizedReplayBuffer
+from trainer.trainer import Trainer
 
 if __name__ == "__main__":
 
@@ -27,60 +28,41 @@ if __name__ == "__main__":
         policy = StandardPolicy(encoder, 50, num_programs, num_non_primary_programs, 100,
                         20, indices_non_primary_programs)
 
+        trainer = Trainer(policy, buffer)
+
         mcts = MCTSExact(env, policy, 1)
     else:
         policy = None
         buffer = None
         mcts = None
+        trainer = None
+        env = None
 
     actor_losses = 0
     critic_losses = 0
     arguments_losses = 0
 
-    for episode in range(10):
+    for iteration in range(2):
 
-        mcts = comm.bcast(mcts, root=0)
+        if rank==0:
+            mcts = MCTSExact(env, policy, 1)
 
-        traces = mcts.sample_execution_trace()
+        for episode in range(3):
 
-        traces = comm.gather(traces, root=0)
+            mcts = comm.bcast(mcts, root=0)
+
+            traces = mcts.sample_execution_trace()
+
+            traces = comm.gather(traces, root=0)
+
+            if rank == 0:
+
+                act_loss, crit_loss, args_loss = trainer.train_one_step(traces)
+
+                print(f"Done {episode}: {len(traces)}: (Buffer size {buffer.get_memory_length()})")
 
         if rank == 0:
 
-            #traces = [len(t.observations) for t in traces]
-
-            # Loop over the traces and save them
-            for t in traces:
-
-                observations = t.observations
-                prog_indices = t.programs_index
-                lstm_states = t.lstm_states
-                policy_labels = t.mcts_policies
-                rewards = t.rewards
-                program_args = t.program_arguments
-
-                if t.clean_sub_execution:
-                    # Generates trace
-                    trace = list(zip(observations, prog_indices, lstm_states, policy_labels, rewards, program_args))
-                    # Append trace to buffer
-                    buffer.append_trace(trace)
-                else:
-                    print("Trace has not been stored in buffer.")
-
-            batch_size = 10
-            num_updates_per_episode = 5
-
-            if buffer.get_memory_length() > batch_size:
-                for _ in range(num_updates_per_episode):
-                    batch = buffer.sample_batch(batch_size)
-                    if batch is not None:
-                        actor_loss, critic_loss, arg_loss, _ = policy.train_on_batch(batch, False)
-                        actor_losses += actor_loss
-                        critic_losses += critic_loss
-                        arguments_losses += arg_loss
-
-                        print(actor_loss, critic_loss, arg_loss)
-
-            print(f"Done {episode}: {len(traces)} (Buffer size {buffer.get_memory_length()})")
+            print(f"[**] Done with iteration {iteration}")
 
 
---
+++ trainer/trainer.py
@@ -0,0 +1,46 @@
+
+class Trainer:
+
+    def __init__(self, policy, buffer, batch_size=10, num_updates_per_episode=5):
+
+        self.policy = policy
+        self.buffer = buffer
+        self.batch_size = batch_size
+        self.num_updates_per_episode = num_updates_per_episode
+
+    def train_one_step(self, traces):
+
+        actor_losses = 0
+        critic_losses = 0
+        arguments_losses = 0
+
+        # Loop over the traces and save them
+        for t in traces:
+
+            observations = t.observations
+            prog_indices = t.programs_index
+            lstm_states = t.lstm_states
+            policy_labels = t.mcts_policies
+            rewards = t.rewards
+            program_args = t.program_arguments
+
+            if t.clean_sub_execution:
+                # Generates trace
+                trace = list(zip(observations, prog_indices, lstm_states, policy_labels, rewards, program_args))
+                # Append trace to buffer
+                self.buffer.append_trace(trace)
+            else:
+                # TODO: better logging
+                print("Trace has not been stored in buffer.")
+
+        if self.buffer.get_memory_length() > self.batch_size:
+            for _ in range(self.num_updates_per_episode):
+                batch = self.buffer.sample_batch(self.batch_size)
+                if batch is not None:
+                    actor_loss, critic_loss, arg_loss, _ = self.policy.train_on_batch(batch, False)
+                    actor_losses += actor_loss
+                    critic_losses += critic_loss
+                    arguments_losses += arg_loss
+
+        return actor_losses/self.num_updates_per_episode, critic_losses/self.num_updates_per_episode, \
+            arguments_losses/self.num_updates_per_episode
